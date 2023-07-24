f959148c8fb9fec26eba181421f1e02ddf3f51c1
Taegwan Youn <tkyoun0421@naver.com>
2023-07-24 20:58:05 +0900

    Feat: banner 텍스트 영역 구현

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import './styles/main.scss'
 // 메뉴바 오픈
 const menuBarsEl = document.querySelector('.menu-bars')
 const headerEl = document.querySelector('#header')
+const headerInnerEl = document.querySelector('#header inner')
 
-menuBarsEl.addEventListener('click', onMenuBar);
+menuBarsEl.addEventListener('click', onMenuBar)
 
-function onMenuBar () {
-    headerEl.classList.toggle('on')
+function onMenuBar (event) {
+    event.stopPropagation()
+    if (headerEl.classList.contains('on')) {
+        headerEl.classList.remove('on')
+    } else {
+        headerEl.classList.add('on')
+    }
 }
+
+headerInnerEl.addEventListener('click', function(event) {
+    event.stopPropagation()
+})
+window.addEventListener('click', function() {
+    headerEl.classList.remove('on')
+});
