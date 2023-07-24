d972e06996e39ad9f04af81601365e25dbaf3908
Taegwan Youn <tkyoun0421@naver.com>
2023-07-24 19:09:05 +0900

    Feat: 헤더 부분 완성

--- dist/maindb3627158805810cb9c2.js
+++
@@ -1,2 +0,0 @@
-console.log("@");
-//# sourceMappingURL=maindb3627158805810cb9c2.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('@')
+import './styles/main.scss'
+
+// 메뉴바 오픈
+const menuBarsEl = document.querySelector('.menu-bars')
+const headerEl = document.querySelector('#header')
+
+menuBarsEl.addEventListener('click', onMenuBar);
+
+function onMenuBar () {
+    headerEl.classList.toggle('on')
+}
