49078e3f9c09d04eb1a7ccbe7fb9b83c133a253c
Taegwan Youn <tkyoun0421@naver.com>
2023-07-25 00:27:08 +0900

    Feat: Home Section 완성

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './styles/main.scss'
 // 메뉴바 오픈
 const menuBarsEl = document.querySelector('.menu-bars')
 const headerEl = document.querySelector('#header')
-const headerInnerEl = document.querySelector('#header inner')
 
 menuBarsEl.addEventListener('click', onMenuBar)
 
@@ -16,9 +15,34 @@ function onMenuBar (event) {
     }
 }
 
-headerInnerEl.addEventListener('click', function(event) {
+// 새로 고침시 맨 위로
+// window.onload = () => {
+//     setTimeout(() => {scrollTo(0,0)},100);
+// }
+
+headerEl.addEventListener('click', function(event) {
     event.stopPropagation()
 })
+
 window.addEventListener('click', function() {
     headerEl.classList.remove('on')
 });
+
+// Intersection Observer
+const sectionEls = [...document.querySelectorAll('.sec')]
+const option = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+}
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('on')
+        }
+    })
+}
+
+const observer = new IntersectionObserver(callback, option)
+sectionEls.forEach(item => observer.observe(item))
